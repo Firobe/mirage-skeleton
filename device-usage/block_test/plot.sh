5ef688011cba3ce9d38a107f9ff101223e8e09f1
Virgile Robles <virgile@tarides.com>
2025-02-12 23:59:07 +0100

    nanoseconds to seconds

--- device-usage/block_test/plot.sh
+++ device-usage/block_test/plot.sh
@@ -3,14 +3,14 @@ cat << EOF > /tmp/plot
 set title "Time to copy 10M of random data (avg. over 10 runs)"
 set logscale x 10
 set xlabel "buffer size (number of sectors)"
-set ylabel "time (ns)"
-set yrange [0:200000000]
+set ylabel "time (s)"
+set yrange [0:0.2]
 plot \
-    'hvt-true.dat' with lp title 'solo5-hvt parallel', \
-    'hvt-false.dat' with lp title 'solo5-hvt serial', \
-    'spt-true.dat' with lp title 'solo5-spt parallel', \
-    'spt-false.dat' with lp title 'solo5-spt serial', \
-    'qemu-true.dat' with lp title 'mirage-unikraft parallel', \
-    'qemu-false.dat' with lp title 'mirage-unikraft serial'
+    'hvt-true.dat' u 1:(\$2/1000000000) with lp title 'solo5-hvt parallel', \
+    'hvt-false.dat' u 1:(\$2/1000000000) with lp title 'solo5-hvt serial', \
+    'spt-true.dat' u 1:(\$2/1000000000) with lp title 'solo5-spt parallel', \
+    'spt-false.dat' u 1:(\$2/1000000000) with lp title 'solo5-spt serial', \
+    'qemu-true.dat' u 1:(\$2/1000000000) with lp title 'mirage-unikraft parallel', \
+    'qemu-false.dat' u 1:(\$2/1000000000) with lp title 'mirage-unikraft serial'
 EOF
 gnuplot --persist /tmp/plot
