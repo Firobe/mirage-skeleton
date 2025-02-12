f1c6e6045a2de6a2a94275069ace1fc5906f483b
Virgile Robles <virgile.robles@protonmail.ch>
2025-02-12 01:58:24 +0100

    wip

--- device-usage/block_test/bench.sh
+++ device-usage/block_test/bench.sh
@@ -3,13 +3,10 @@ EXT=hvt
 FILESIZE=100M
 make build
 for parallel in true false; do
-    rm xdata
-    touch xdata
     hyperfine \
         -L BUFFSIZE 1,2,5,10,50,100,200,500,1000,2000 \
         --prepare "dd if=/dev/urandom of=storage1 bs=$FILESIZE count=1 iflag=fullblock && \
-                   dd if=/dev/urandom of=storage2 bs=$FILESIZE count=1 iflag=fullblock && \
-                   echo {BUFFSIZE} >> xdata" \
+                   dd if=/dev/urandom of=storage2 bs=$FILESIZE count=1 iflag=fullblock" \
         --export-csv "parallel_$parallel.csv" \
         "solo5-$EXT \
             --block:storage1=storage1 \
